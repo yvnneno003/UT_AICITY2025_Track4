e76895b2f77dd87263e715a7e9e72a9d6091a48d
Lily Wu <m11016023@go.utaipei.edu.tw>
2025-07-16 11:00:33 +0800

    add source code

--- dataprocessing/8k-random-split.py
+++ dataprocessing/8k-random-split.py
@@ -2,42 +2,42 @@ import os
 import shutil
 from sklearn.model_selection import train_test_split
 
-# 設定圖檔和標籤檔的資料夾路徑
-image_dir = '/home/user/WDC2TB/m11016023/T4/FishEye_Object_Detection/datasets/8k/images'
-label_dir = '/home/user/WDC2TB/m11016023/T4/FishEye_Object_Detection/datasets/8k/labels'
-train_image_dir = '/home/user/WDC2TB/m11016023/T4/FishEye_Object_Detection/datasets/8k/train/images'
-val_image_dir = '/home/user/WDC2TB/m11016023/T4/FishEye_Object_Detection/datasets/8k/val/images'
-train_label_dir = '/home/user/WDC2TB/m11016023/T4/FishEye_Object_Detection/datasets/8k/train/labels'
-val_label_dir = '/home/user/WDC2TB/m11016023/T4/FishEye_Object_Detection/datasets/8k/val/labels'
 
-# 創建訓練集和驗證集的資料夾
+image_dir = 'datasets/8k/images'
+label_dir = 'datasets/8k/labels'
+train_image_dir = 'datasets/8k/train/images'
+val_image_dir = 'datasets/8k/val/images'
+train_label_dir = 'datasets/8k/train/labels'
+val_label_dir = 'datasets/8k/val/labels'
+
+
 os.makedirs(train_image_dir, exist_ok=True)
 os.makedirs(val_image_dir, exist_ok=True)
 os.makedirs(train_label_dir, exist_ok=True)
 os.makedirs(val_label_dir, exist_ok=True)
 
-# 獲取圖檔中的所有檔名
+
 image_files = [f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))]
 
-# 使用 train_test_split 進行分割
+
 train_files, val_files = train_test_split(image_files, test_size=0.3, random_state=42)
 
-# 將圖檔及其對應的標籤檔案移動到相應的資料夾
+
 def move_files_and_labels(file_list, target_image_dir, target_label_dir):
     for file_name in file_list:
-        # 移動圖片
+
         src_image = os.path.join(image_dir, file_name)
         dst_image = os.path.join(target_image_dir, file_name)
         shutil.move(src_image, dst_image)
         
-        # 移動對應的標籤檔案
+
         label_file = file_name.rsplit('.', 1)[0] + '.txt'
         src_label = os.path.join(label_dir, label_file)
         if os.path.isfile(src_label):
             dst_label = os.path.join(target_label_dir, label_file)
             shutil.move(src_label, dst_label)
 
-# 移動文件到訓練集和驗證集資料夾
+
 move_files_and_labels(train_files, train_image_dir, train_label_dir)
 move_files_and_labels(val_files, val_image_dir, val_label_dir)
 
